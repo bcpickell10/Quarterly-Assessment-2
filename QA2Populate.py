dc818fe70039dc2f0ce8449206a2b10de3cafa63
YourName <youremail@website.com>
2024-03-06 09:44:35 -0600

    Updated with populated Analytics table

--- QA2Populate.py
+++ QA2Populate.py
@@ -19,3 +19,15 @@ cr.execute(
     (5,'True/False: A high debt-to-equity ratio indicates a low level of financial risk.','False')"""
     )
 
+cr.execute(
+    """INSERT INTO Analytics VALUES
+    (1,'True/False: In regression analysis, the dependent variable is the variable being predicted or explained by the independent variable.','True'),
+    (2,'In simple linear regression, the equation of the regression line is represented as Y = a + βX + ε, where a is the ________ and β is the regression coefficient.','intercept'),
+    (3,'True/False: A p-value less than the significance level indicates that the null hypothesis should be rejected.','True'),
+    (4,'The regression coefficient represents the ________ in the dependent variable for a one-unit change in the independent variable.','change'),
+    (5,' If the regression coefficient of a variable is negative, what does it indicate?
+    a. There is no relationship between the variables
+    b. An increase in the independent variable is associated with a decrease in the dependent variable
+    c. An increase in the independent variable is associated with an increase in the dependent variable
+    d. The coefficient is not interpretable','c')"""
+)
